14ea6cd091c0f33581871086aae245196fa3b029
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-01-30 20:05:25 +0530

    who are we section created.

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import HeroSection from "../components/HeroSection.jsx";
 import About from "../components/About.jsx";
 import Qualities from "../components/Qualities.jsx";
 import Menu from "../components/Menu.jsx"
+import WhoAreWe from "../components/WhoAreWe.jsx";
 
 const Home = () => {
     return ( 
@@ -11,6 +12,7 @@ const Home = () => {
             <About/>
             <Qualities />
             <Menu />
+            <WhoAreWe />
         </>
      );
 }
